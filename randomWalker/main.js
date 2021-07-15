64d89320858121576cdbf480954d0c272bb7f8e6
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-07-15 22:38:17 +0700

    fix move bug when position over border

--- randomWalker/main.js
+++ randomWalker/main.js
@@ -1,6 +1,6 @@
 const width = 500;
 const height = 500;
-const resolution = 2;
+const resolution = 10;
 let x;
 let y;
 
@@ -10,7 +10,7 @@ function setup(){
 
 function draw(){
     fill(200);
-    rect(x,y,10,10);
+    rect(x,y,resolution,resolution);
     move();
 }
 
@@ -24,7 +24,7 @@ function init(){
     stroke(0);
     rect(0, 0, width, height);
     x = width / 2 - (resolution / 2);
-    y = height / 2 - (resolution / 2);
+    y = height / 2 - (resolution / 2);    
 }
 
 function move(){
@@ -32,7 +32,7 @@ function move(){
     let additionalY = 0;
     const posibilityMoves = [2, -2];
 
-    if(x <= 0){
+    if(x <= resolution){
         additionalX = posibilityMoves[0];
     }else if(x >= width - resolution){
         additionalX = posibilityMoves[1]
@@ -40,10 +40,10 @@ function move(){
         additionalX = posibilityMoves[floor(random(2))];
     }
 
-    if(y <= 0){
+    if(y <= resolution){
         additionalY = posibilityMoves[0];
-    }else if(x >= width - resolution){
-        additionalY = posibilityMoves[1]
+    }else if(y >= height - resolution){
+        additionalY = posibilityMoves[1];
     }else{
         additionalY = posibilityMoves[floor(random(2))];
     }
